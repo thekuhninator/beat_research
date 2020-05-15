e2f69856141759a03fdeba749b9ebeb6c008669f
thekuhninator <roman.kuhn1@gmail.com>
2020-05-15 11:19:19 -0500

    run beat and run_multibeat.sh now work quite well

---
+++ run_beat.sh
@@ -0,0 +1,6 @@
+./beat.R -g example_data/dataset_1/dataset1_gene_counts.csv -a example_data/dataset_1
+/dataset1_metadata.csv -u -o  output/dataset1/dataset1_uncorrected -f diagnosis -d data
+set1_
+uncorrected
+
+
---
+++ run_multibeat.sh
@@ -0,0 +1,1 @@
+./multi_beat.R -d ../ -o aggregate -n dataset1
